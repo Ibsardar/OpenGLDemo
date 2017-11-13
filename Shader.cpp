8801f5b670d47ebae116902d46e645108c7a0a63
Ibrahim Sardar <ibrysar@gmail.com>
2017-11-13 01:15:44 -0500

    implemented some more shader stuff

--- Shader.cpp
+++ Shader.cpp
@@ -1,22 +1,93 @@
 #include "Shader.h"
+#include <iostream>
+#include <fstream>
+
+/// Define helper functions here: ------
+static std::string LoadShader(const std::string & filename);
+static void CheckShaderError(GLuint shader, GLuint flag, bool is_program,
+	const std::string & error_msg);
+/// ------------------------------------
 
 Shader::Shader() {
 
+	// ...
+}
+
+Shader::Shader(const std::string & filename) {
+
+	// store program handle after creating the program
+	this->m_program = glCreateProgram();
+	
+	// HERE!!!
 }
 
 Shader::Shader(const Shader& shader) {
 
+	// free memory of created program
+	glDeleteProgram(this->m_program);
 }
 
 Shader::~Shader() {
 
 }
 
-Shader& Shader::operator=(const Shader& shader) {
+Shader& Shader::operator=(const Shader& rhs) {
 
 	// self assignment
 	if (this == &rhs)
 		return *this;
 
 	return *this;
+}
+
+//
+//	Helper functions below:
+//
+
+// loads a shader file
+static std::string LoadShader(const std::string & filename) {
+
+	std::ifstream file;
+	file.open(filename.c_str());
+
+	std::string output;
+	std::string line;
+
+	if (file.is_open()) {
+		
+		while (file.good()) {
+			
+			getline(file, line);
+			output.append(line + "\n");
+		}
+
+	} else {
+
+		std::cerr << "Unable to load shader: " << filename << std::endl;
+	}
+
+	return output;
+}
+
+// checks for shader errors
+void CheckShaderError(GLuint shader, GLuint flag, bool is_program,
+	const std::string & error_msg) {
+
+	GLint success = 0;
+	GLchar error[1024] = { 0 };
+
+	if (is_program)
+		glGetProgramiv(shader, flag, &success);
+	else
+		glGetShaderiv(shader, flag, &success);
+
+	if (success == GL_FALSE) {
+
+		if (is_program)
+			glGetProgramInfoLog(shader, sizeof(error), NULL, error);
+		else
+			glGetShaderInfoLog(shader, sizeof(error), NULL, error);
+
+		std::cerr << error_msg << ": '" << error << "'" << std::endl;
+	}
 }
--- Shader.h
+++ Shader.h
@@ -1,21 +1,38 @@
 #ifndef SHADER_H
 #define SHADER_H
 
+#include <string>
+#include <GL/glew.h>
+
 class Shader {
 
 public:
 	// methods
 	Shader();
+	Shader(const std::string & filename);
 	virtual ~Shader();
+
+	// set the GPU to use the vertex and fragment shaders from this class
+	void bind();
 
 protected:
 
 private:
 	// methods
-	Shader(const Shader& shader);
-	Shader& operator=(const Shader& shader);
+	Shader(const Shader&);
+	Shader& operator=(const Shader&);
 
-}; // Shader
+	// variables
+	// 2 for the vertex shader and the fragment shader
+	static const unsigned int NUM_SHADER = 2;
+
+	// keeps track of program handle
+	GLuint m_program;
+
+	// list of types of shaders (only vertex and fragment for now..)
+	GLuint m_shaders[NUM_SHADER];
+
+}; //end Shader
 
 #endif // SHADER_H
 
